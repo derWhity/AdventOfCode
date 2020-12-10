215a3bffafca512ac21d85ae4baef3355a03aa0d
Stefan Westphal <whity@fressbrett.net>
2020-12-10 10:34:39 +0100

    Replace day 10 star 2 with a more seasoned non-bruteforce variant

--- 2020/day_10/star_02/main.go
+++ 2020/day_10/star_02/main.go
@@ -8,34 +8,6 @@ import (
 	"github.com/derWhity/AdventOfCode/lib/input"
 )
 
-func countPossibilities(items []int, collected []int) uint64 {
-	var out uint64
-	coll := []int{}
-	coll = append(coll, collected...)
-	coll = append(coll, items[0])
-	if len(items) == 1 {
-		// We are at the last item - so we found a match
-		//fmt.Printf("Match: %+v\n", collected)
-		return 1
-	}
-	if len(items) >= 2 {
-		if items[1]-items[0] <= 3 {
-			out += countPossibilities(items[1:], coll)
-		}
-	}
-	if len(items) >= 3 {
-		if items[2]-items[0] <= 3 {
-			out += countPossibilities(items[2:], coll)
-		}
-	}
-	if len(items) >= 4 {
-		if items[3]-items[0] <= 3 {
-			out += countPossibilities(items[3:], coll)
-		}
-	}
-	return out
-}
-
 func main() {
 	fmt.Println("-------------")
 	items, err := input.ReadIntNative(filepath.Join("..", "input.txt"))
@@ -43,12 +15,13 @@ func main() {
 		panic(err)
 	}
 	// Sort the adapters by joltage
-	items = append(items, 0) // Add the seat outlet
 	iSlice := sort.IntSlice(items)
 	iSlice.Sort()
 	iSlice = append(iSlice, iSlice[len(iSlice)-1]+3) // Add our own device
 	fmt.Printf("Slice: %+v\n", iSlice)
-	// Counting recursively will take a looooooong time - time to grab some coffee
-	count := countPossibilities(iSlice, []int{})
-	fmt.Printf("Different possibilities %d\n", count)
+	pathsTo := map[int]int{0: 1}
+	for _, v := range iSlice {
+		pathsTo[v] = pathsTo[v-1] + pathsTo[v-2] + pathsTo[v-3]
+	}
+	fmt.Printf("Different possibilities %d\n", pathsTo[iSlice[len(iSlice)-1]])
 }
