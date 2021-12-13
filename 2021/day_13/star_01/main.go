de56e9f1d5f248ef69cc13b30acc78690e94bf66
Stefan Westphal <whity@fressbrett.net>
2021-12-13 21:27:43 +0100

    Add day 13 of 2021

---
+++ 2021/day_13/star_01/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/derWhity/AdventOfCode/lib/input"
+	"github.com/disintegration/imaging"
+	"github.com/spf13/cast"
+)
+
+var (
+	foldRegex = regexp.MustCompile(`^fold along ([xy])=([0-9]+)$`)
+)
+
+func failOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	items, err := input.ReadString(filepath.Join("..", "input.txt"), true)
+	failOnError(err)
+	// Determine the size of the canvas
+	var width, height int
+	for _, line := range items {
+		if line == "" {
+			// Folding section begins
+			break
+		}
+		coords := strings.Split(line, ",")
+		x := cast.ToInt(coords[0])
+		if x > width {
+			width = x
+		}
+		y := cast.ToInt(coords[1])
+		if y > height {
+			height = y
+		}
+
+	}
+	canvas := image.NewNRGBA(image.Rect(0, 0, width+1, height+1))
+	fmt.Printf("Base image size: %dx%d\n", canvas.Rect.Dx(), canvas.Rect.Dy())
+	red := color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+	folding := false
+	for i, line := range items {
+		if !folding {
+			// Coordinate plotting
+			if line == "" {
+				// Skip to the folding section
+				folding = true
+				continue
+			}
+			coords := strings.Split(line, ",")
+			x := cast.ToInt(coords[0])
+			y := cast.ToInt(coords[1])
+			canvas.Set(x, y, red)
+		} else if line != "" {
+			fmt.Println(line)
+			// Folding section
+			var newCanvas *image.NRGBA
+			var foldedPart *image.NRGBA
+			match := foldRegex.FindStringSubmatch(line)
+			pos := cast.ToInt(match[2])
+			if match[1] == "x" {
+				// Vertical split
+				newCanvas = imaging.Crop(imaging.Clone(canvas), image.Rect(0, 0, pos, canvas.Rect.Dy()))
+				foldedPart = imaging.Crop(imaging.Clone(canvas), image.Rect(pos+1, 0, canvas.Rect.Dx(), canvas.Rect.Dy()))
+				foldedPart = imaging.FlipH(foldedPart)
+			} else {
+				// Horizontal split
+				newCanvas = imaging.Crop(imaging.Clone(canvas), image.Rect(0, 0, canvas.Rect.Dx(), pos))
+				foldedPart = imaging.Crop(imaging.Clone(canvas), image.Rect(0, pos+1, canvas.Rect.Dx(), canvas.Rect.Dy()))
+				foldedPart = imaging.FlipV(foldedPart)
+			}
+			canvas = imaging.Overlay(imaging.Clone(newCanvas), imaging.Clone(foldedPart), image.Pt(0, 0), 1.0)
+			fmt.Printf("New image size: %dx%d\n", canvas.Rect.Dx(), canvas.Rect.Dy())
+			w, err := os.Create(fmt.Sprintf("out_%d.png", i))
+			failOnError(err)
+			defer w.Close()
+			png.Encode(w, canvas)
+			break
+		}
+	}
+	// Count the red pixels
+	counter := 0
+	for x := 0; x < canvas.Rect.Dx(); x++ {
+		for y := 0; y < canvas.Rect.Dy(); y++ {
+			if canvas.NRGBAAt(x, y) == red {
+				counter++
+			}
+		}
+	}
+	fmt.Printf("Count: %d\n", counter)
+
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.17
 
 require (
 	github.com/dhconnelly/rtreego v1.1.0
+	github.com/disintegration/imaging v1.6.2
 	github.com/logrusorgru/aurora/v3 v3.0.0
 	github.com/spf13/cast v1.4.1
 )
+
+require golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
